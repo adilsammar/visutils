50b0c483cd8bed13fc7c2e722a191d06279ed35c
Ammar Adil <ammaradil80@gmail.com>
2019-10-04 12:38:05 +0530

    Update normalize to just accept image as parameter

--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 
 def normalize(mean=[0.4913997551666284, 0.48215855929893703, 0.4465309133731618],
               std=[0.24703225141799082, 0.24348516474564, 0.26158783926049628]):
-    return lambda x, y: (tf.math.divide(tf.math.subtract(x, mean), std), y)
+    return lambda x: tf.math.divide(tf.math.subtract(x, mean), std)
 
